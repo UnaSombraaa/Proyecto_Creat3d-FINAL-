e43f8c26051cbc9a8578408d4864df9bb6877851
UnaSombraaa <127241057+UnaSombraaa@users.noreply.github.com>
2023-10-09 17:18:11 -0300

    NewCommit Style

--- project/home.php
+++ project/home.php
@@ -21,10 +21,10 @@ if(isset($_POST['add_to_cart'])){
    $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
 
    if(mysqli_num_rows($check_cart_numbers) > 0){
-      $message[] = 'already added to cart!';
+      $message[] = 'Ya fue añadido al carrito!';
    }else{
       mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, quantity, image) VALUES('$user_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
-      $message[] = 'product added to cart!';
+      $message[] = 'Producto añadido al carrito!';
    }
 
 }
